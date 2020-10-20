01f11872e7af0fb4f99d394c01f83593e22ccf55
gengbing <edwingeng@126.com>
2020-10-20 12:54:27 +0800

    extend the Logger interface

--- slog.go
+++ slog.go
@@ -1,13 +1,22 @@
 package slog
 
 type Logger interface {
+	NewLoggerWith(args ...interface{}) Logger
+
 	Debug(args ...interface{})
 	Info(args ...interface{})
 	Warn(args ...interface{})
 	Error(args ...interface{})
 
-	Debugf(format string, args ...interface{})
-	Infof(format string, args ...interface{})
-	Warnf(format string, args ...interface{})
-	Errorf(format string, args ...interface{})
+	Debugf(template string, args ...interface{})
+	Infof(template string, args ...interface{})
+	Warnf(template string, args ...interface{})
+	Errorf(template string, args ...interface{})
+
+	Debugw(msg string, keyVals ...interface{})
+	Infow(msg string, keyVals ...interface{})
+	Warnw(msg string, keyVals ...interface{})
+	Errorw(msg string, keyVals ...interface{})
+
+	FlushLogger() error
 }
