ad89ce4dcc569502b0a891989561632470076e81
gengbing <edwingeng@126.com>
2024-05-09 13:12:21 +0800

    rename: DumbLogger >> devourer

---
+++ devourer.go
@@ -0,0 +1,33 @@
+package slog
+
+var (
+	_ Logger = devourer{}
+)
+
+type devourer struct{}
+
+// NewDevourer creates a new Logger that devours all log messages and outputs nothing.
+func NewDevourer() Logger {
+	return devourer{}
+}
+
+func (devourer) NewLoggerWith(keyVals ...any) Logger {
+	return devourer{}
+}
+
+func (devourer) Debug(args ...any) {}
+func (devourer) Info(args ...any)  {}
+func (devourer) Warn(args ...any)  {}
+func (devourer) Error(args ...any) {}
+
+func (devourer) Debugf(format string, args ...any) {}
+func (devourer) Infof(format string, args ...any)  {}
+func (devourer) Warnf(format string, args ...any)  {}
+func (devourer) Errorf(format string, args ...any) {}
+
+func (devourer) Debugw(msg string, keyVals ...any) {}
+func (devourer) Infow(msg string, keyVals ...any)  {}
+func (devourer) Warnw(msg string, keyVals ...any)  {}
+func (devourer) Errorw(msg string, keyVals ...any) {}
+
+func (devourer) FlushLogger() error { return nil }
